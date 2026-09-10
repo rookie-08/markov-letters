d409d3c40e951da90c2fe74cbb54343f819b7022
Rookie <lhn4519@rit.edu>
2026-09-10 12:25:26 -0400

    Outlined main() of markov.py

--- markov.py
+++ markov.py
@@ -1,4 +1,25 @@
+"""
+
+"""
+
+def make_markov_network():
+    ...
+
+def use_markov_network():
+    ...
+
 def main():
+    """
+    Prompts the user to type "make" to make the markov chain or "use" to run the markov chain.
+    """
+    # Set a blank string variable for the input
+    # Get a loop that is broken once the string variable is empty
+    while False:
+        # Prompt the user to type "make" or "use"
+        # If neither is used, repeat
+        ...
+    # If make, invoke `write` and `make_markov_network`
+    # If use, invoke `read`, `use_markov_network`, and `print`
     ...
 
 if (__name__ == "__main__"):
