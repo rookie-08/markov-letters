6f1293444f58ed2cb242a3f6b800b2ccb5b64a79
Rookie <lhn4519@rit.edu>
2026-09-11 11:37:58 -0400

    Partially wrote (and commented out) a test for markov.py's test_analyze_word_to_tokens()

---
+++ markov_test.py
@@ -0,0 +1,15 @@
+import markov.py
+
+# Metatests
+
+# Tests
+"""def test_analyze_word_to_tokens():
+    # setup
+    word = "table"
+    expected = ["START", "t", "t", "a", "ta", "b", "ab", "l", "bl", "e", "le", "END"]
+
+    # invoke
+    actual = markov.analyze_word_to_tokens(word)
+
+    # analyze
+    # assert expected == actual"""
