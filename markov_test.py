0c794754e5d034f1c0490c6a4064cf0d666e3717
Rookie <lhn4519@rit.edu>
2026-09-11 11:44:48 -0400

    Implemented search_list_for_element() of markov_test.py

--- markov_test.py
+++ markov_test.py
@@ -1,14 +1,14 @@
-import markov.py
+import markov
 
 # Auxillary
 def search_list_for_element(a_list, an_element):
     """
     Returns `True` if `an_element` (preferably not a list) can be found in `a_list`.
     """
-    # Run a for loop.
-        # Is each element equal to an_element?
-            # Return true.
-    # Return false.
+    for element_of_list in a_list:
+        if (element_of_list == an_element):
+            return True
+    return False
 
 def compare_matching_lists(list_a, list_b):
     """
@@ -20,6 +20,41 @@ def compare_matching_lists(list_a, list_b):
     ...
 
 # Metatests
+def test_search_list_for_element_abcde_d():
+    # setup
+    a_list = ["a", "b", "c", "d", "e"]
+    an_element = "d"
+    expected = True
+
+    # invoke
+    actual = search_list_for_element(a_list, an_element)
+
+    # analyze
+    assert expected == actual
+
+def test_search_list_for_element_abcde_g():
+    # setup
+    a_list = ["a", "b", "c", "d", "e"]
+    an_element = "g"
+    expected = False
+
+    # invoke
+    actual = search_list_for_element(a_list, an_element)
+
+    # analyze
+    assert expected == actual
+
+def test_search_list_for_element_words():
+    # setup
+    a_list = ["START", "ab", "END"]
+    an_element = "END"
+    expected = True
+
+    # invoke
+    actual = search_list_for_element(a_list, an_element)
+
+    # analyze
+    assert expected == actual
 
 # Tests
 """def test_analyze_word_to_tokens():
